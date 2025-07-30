8f76cd0ecd18f8600eeff3d5ededb5f90b41f8c6
CartezDew <cartez.dewberry@gmail.com>
2025-07-29 23:37:31 -0400

    removed rollup-plugin-terser package

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { terser } from 'rollup-plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks: {
